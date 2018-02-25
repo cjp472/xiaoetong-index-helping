651cf36cfef35214d4f21b7a8152d42b39d8c723
xiaoshuzai <280470115@qq.com>
2018-02-25 20:24:53 +0800

    index&helping

---
+++ src/view/helping.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <xheader></xheader>
+    <xnav></xnav>
+    <router-view/>
+    <xfooter></xfooter>
+    <backToTop></backToTop>
+  </div>
+</template>
+
+<script>
+import xfooter from 'helping/xfooter/xfooter'
+import xheader from 'helping/xheader/xheader'
+import xnav from 'helping/xnav/xnav'
+import backToTop from '@/components/common/backToTop/backToTop'
+
+export default {
+  components: {
+    xfooter,
+    xheader,
+    xnav,
+    backToTop
+  }
+}
+</script>
